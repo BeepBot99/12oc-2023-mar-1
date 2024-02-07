530ea8555eac677c83680ebdd011491d62bd34cb
BeepBot99 <Davis.Luxenberg@outlook.com>
2024-02-06 20:45:33 -0600

    Did some testing

--- script.js
+++ script.js
@@ -5,8 +5,7 @@
 // Read ahead at your own risk.
 // ----------
 
-const STARTING_URL = "https://instituto0.wordpress.com/";
-const NTH_LAST_URL = 1;
+const STARTING_URL = "https://this-is-benfica.tumblr.com/";
 const ITERATIONS = 10;
 
 const puppeteer = require("puppeteer");
@@ -14,7 +13,7 @@ async function runProgram() {
     let currentURL = STARTING_URL;
     for (let i = 0; i < ITERATIONS; i++) {
         console.log(currentURL, `Iteration ${i}/${ITERATIONS}`);
-        currentURL = await evaluateURL(currentURL, NTH_LAST_URL);
+        currentURL = await evaluateURL(currentURL, 1);
     }
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -28,6 +27,12 @@ async function evaluateURL(urlToEvaluate, n) {
     const page = await browser.newPage();
     let href;
     await page.goto(urlToEvaluate);
+    // console.log(
+    //     await page.evaluate(
+    //         `document.getElementsByTagName("a")[document.getElementsByTagName("a").length-${n}]`
+    //     ),
+    //     n
+    // );
     if (
         await page.evaluate(
             `document.getElementsByTagName("a")[document.getElementsByTagName("a").length-${n}].hasAttribute("href")`
@@ -36,14 +41,15 @@ async function evaluateURL(urlToEvaluate, n) {
         href = await page.evaluate(
             `document.getElementsByTagName("a")[document.getElementsByTagName("a").length-${n}].getAttribute("href")`
         );
+        await browser.close();
         if (href.indexOf("http") !== 0) {
             href = await evaluateURL(urlToEvaluate, n + 1);
         }
     } else {
+        await browser.close();
         href = await evaluateURL(urlToEvaluate, n + 1);
     }
 
-    await browser.close();
     return href;
 }
 runProgram();
